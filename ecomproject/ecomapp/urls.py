7d143bfbfd93e86fc4459570382b767d6cb61399
nimmijith <nimmiak84@gmail.com>
2024-08-06 13:51:36 +0530

    added

---
+++ ecomproject/ecomapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'ecomapp'
+urlpatterns = [
+    path('', views.allProdCat, name='allProductCat'),
+    path('<slug:c_slug>/slug', views.allProdCat, name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/', views.prodDetail, name='prodDetail'),
+]
